5ea8ec9d0a645cd2fe346b8ed0164cd6d87f3020
Jatin Sharma <Jatin.sharma7983@gmail.com>
2019-06-16 02:35:37 +0530

    Update

--- app/src/main/java/com/example/project2/HomeActivity.java
+++ app/src/main/java/com/example/project2/HomeActivity.java
@@ -1,4 +1,21 @@
 package com.example.project2;
 
-public class HomeActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activity);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
 }
