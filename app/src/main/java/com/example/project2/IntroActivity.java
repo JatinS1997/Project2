a2300bbf34b5cb4a435b12fee3248086c1224623
Jatin Sharma <Jatin.sharma7983@gmail.com>
2019-06-17 00:33:58 +0530

    Button Working

--- app/src/main/java/com/example/project2/IntroActivity.java
+++ app/src/main/java/com/example/project2/IntroActivity.java
@@ -2,7 +2,9 @@ package com.example.project2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,6 +13,11 @@ public class IntroActivity extends Activity {
 
 
     Button button;
+    private AnimationDrawable animationDrawable;
+    private ConstraintLayout constraintLayout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,18 @@ public class IntroActivity extends Activity {
 
             }
         });
+
+        // init constraintLayout
+        constraintLayout = (ConstraintLayout) findViewById(R.id.layout);
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(5000);
+
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(2000);
     }
 
 
@@ -39,5 +58,24 @@ public class IntroActivity extends Activity {
             startActivity(i);
 
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
     }
 }
--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.project2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
@@ -21,6 +22,8 @@ public class MainActivity extends Activity {
     private ConstraintSet constraintSet2 = new ConstraintSet();
     private boolean altLayout;
     Button button;
+    private AnimationDrawable animationDrawable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,18 @@ public class MainActivity extends Activity {
 
             }
         });
+
+        // init constraintLayout
+        constraintLayout = (ConstraintLayout) findViewById(R.id.layout);
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(5000);
+
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(2000);
 
 
 
@@ -71,6 +86,25 @@ public class MainActivity extends Activity {
             altLayout=false;
         }
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
     }
 
 
