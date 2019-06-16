4f06f591a1ee126dbc074d27578b9c42c2affa71
Jatin Sharma <Jatin.sharma7983@gmail.com>
2019-06-16 14:27:06 +0530

    Update

---
+++ app/src/main/java/com/example/project2/FragmentMessages.java
@@ -0,0 +1,25 @@
+package com.example.project2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMessages extends Fragment {
+
+
+    View view;
+    public FragmentMessages() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view=inflater.inflate(R.layout.messages_fragment,container,false);
+        return view;
+    }
+}
