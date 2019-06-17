b319a11d1dfe2d8c0f00e1930af25d78ebfee641
Jatin Sharma <Jatin.sharma7983@gmail.com>
2019-06-17 23:34:07 +0530

    Bottom Not Working

--- app/src/main/java/com/example/project2/HomeActivity.java
+++ app/src/main/java/com/example/project2/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.example.project2;
 
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,12 +12,19 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
     public Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+    private ConstraintLayout constraintLayout;
+    private FragmentCalls fragmentCalls;
+    private FragmentMessages fragmentMessages;
+    private FragmentHistory fragmentHistory;
+
 
 
     @Override
@@ -39,10 +49,64 @@ public class HomeActivity extends AppCompatActivity {
 
         ListFragment fragment = new ListFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.placeholder, fragment);
         fragmentTransaction.commit();
+
+
+
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.placeholder);
+        fragmentCalls = new FragmentCalls();
+        fragmentMessages = new FragmentMessages();
+        fragmentHistory = new FragmentHistory();
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+
+                switch (menuItem.getItemId())
+                {
+
+                    case R.id.nav_calls :
+                        bottomNavigationView.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(fragmentCalls);
+                        return true;
+
+                    case R.id.nav_messages:
+                        setFragment(fragmentMessages);
+                        return true;
+
+                    case R.id.nav_history:
+                        setFragment(fragmentHistory);
+                        return true;
+
+
+
+                }
+                return true;
+            }
+
+            private void setFragment(Fragment fragment) {
+
+                FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+
+                fragmentTransaction.replace(R.id.placeholder, fragmentCalls);
+                fragmentTransaction.commit();
+            }
+
+        });
+
+
+
+
+
     }
+
+
 
 
 }
